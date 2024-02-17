73d99c7bbfe7b7fa00de9b6f1d30b7a293e1e542
Aqilah <aqilahfatinfauzi@gmail.com>
2024-02-17 23:17:27 +0800

    displayed all photos

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,31 +1,100 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import axios from 'axios';
+import { Image, Text, View, Alert, FlatList } from 'react-native';
+import MasonryList from '@react-native-seoul/masonry-list';
 
 interface Photo {
   id: string;
   title: string;
   url: string;
-  thumbailUrl: string;
+  thumbnailUrl: string;
   albumId: string;
 }
+
+interface Props {
+  photo: Photo;
+}
+
+const PhotoCard = (props: Props) => {
+  const { photo } = props;
+  return (
+    <View
+      className="mb-4"
+      style={{ position: 'relative', flexShrink: 1, width: '48%' }}
+    >
+      <Image
+        source={{ uri: photo.thumbnailUrl }}
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: 250,
+          borderRadius: 20,
+        }}
+      />
+      <Text>{photo.title}</Text>
+    </View>
+  );
+};
 
 const ImageGallery = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
 
-  const fetchImages = async () => {
-    await fetch('https://jsonplaceholder.typicode.com/photos')
-      .then((response) => response.json())
-      .then((json) => setPhotos(json));
-  };
-
   useEffect(() => {
     fetchImages();
+    console.log('useEffect is working');
   }, []);
+
+  const fetchImages = async () => {
+    await axios
+      .get('https://jsonplaceholder.typicode.com/photos')
+      .then((response: any) => {
+        setPhotos(response.data);
+        console.log('Photos fetched: ', response.data.length);
+      })
+      .catch((error: any) => Alert.alert(error));
+  };
 
   return (
     <View className="pt-4">
-      {photos.length > 0 &&
-        photos.map((photo) => <Text key={photo.id}>{photo.title}</Text>)}
+      <FlatList
+        initialNumToRender={20}
+        numColumns={2}
+        columnWrapperStyle={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+        }}
+        data={photos}
+        renderItem={({ item }) => (
+          <PhotoCard
+            photo={item as Photo}
+            key={item.id}
+          />
+        )}
+      />
+      {/* {photos.length > 0 &&
+          photos.map((photo, i) => (
+            <PhotoCard
+              photo={photo as Photo}
+              index={i}
+              key={photo.id}
+            />
+          ))} */}
+      {/* {photos.length > 0 && (
+        <MasonryList
+          data={photos}
+          keyExtractor={(item): string => item.id}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item, i }) => (
+            <PhotoCard
+              photo={item as Photo}
+              index={i}
+            />
+          )}
+          onEndReachedThreshold={0.1}
+        />
+      )} */}
     </View>
   );
 };
--- src/screens/PhotoScreen.tsx
+++ src/screens/PhotoScreen.tsx
@@ -1,21 +1,20 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import React from 'react';
 
 import ImageGallery from '../components/ImageGallery';
 
 const PhotoScreen = () => {
   return (
-    <View className="flex-1 items-start justify-start bg-[#f2e9e4] pb-[90px] px-4">
+    <ScrollView className="bg-[#f2e9e4] pb-[90px] px-4">
       <Text className="text-[#22223b] mb-2">Hello, human!</Text>
       <Text className="font-bold text-4xl text-[#22223b]">
         Start browsing through our collection of{' '}
         <Text className="font-bold text-4xl text-[#9a8c98]">images</Text>
       </Text>
-
       <View>
         <ImageGallery />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
