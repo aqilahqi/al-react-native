9b2a40129feab2f7e701171a47cc197c6d4ef046
Aqilah <aqilahfatinfauzi@gmail.com>
2024-02-17 20:58:13 +0800

    add app description

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,7 +5,9 @@ function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-[#f5ebe0]">
       <StatusBar style="dark" />
-      <Text>Home Screen</Text>
+      <Text className="font-semibold text-lg mb-2">Welcome 👋🏻</Text>
+      <Text className="font-bold text-4xl mb-10">Photo Gallery</Text>
+      <Text>Browse photos to kill the time</Text>
     </View>
   );
 }
